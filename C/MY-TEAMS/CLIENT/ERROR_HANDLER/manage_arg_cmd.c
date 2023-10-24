4cc701aa9105a207028703479f00cd923bb10e82
Raphael de Monchy <72072819+raphaeldemonchy@users.noreply.github.com>
2023-10-24 12:26:00 +0200

    Delete C/MY-TEAMS directory

--- C/MY-TEAMS/CLIENT/ERROR_HANDLER/manage_arg_cmd.c
+++
@@ -1,6 +0,0 @@
-/*
-** EPITECH PROJECT, 2022
-** MY_TEAMS_NORME1
-** File description:
-** manage_arg_cmd.c
-*/
--- C/MY-TEAMS/CLIENT/ERROR_HANDLER/manage_uuid.c
+++
@@ -1,6 +0,0 @@
-/*
-** EPITECH PROJECT, 2022
-** manage_uuid
-** File description:
-** manage_uuid.c
-*/
